5510b0b629b9c588f9b62b77bcc1e1606ea2282e
Sakshi Ajabe <sakshiajabe4@gmail.com>
2025-02-25 20:12:20 +0530

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,4 +45,31 @@ const registerUser = async (req , res) =>{
     }
 }
 
+//API for user login
+const loginUser = async (req,res) =>{
+    try{
+        const {email,password} = req.body
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            res.json({success:false,message:'user does not exist'})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if(isMatch){
+            const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }
+        else{
+            res.json({success:false,message:"Invalid Credentials"})
+        }
+
+    }
+    catch(error){
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
 export {registerUser}
