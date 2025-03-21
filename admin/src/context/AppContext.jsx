3b78444b9d04ea18df5a5fcece85b02544acbe2b
Sakshi Ajabe <sakshiajabe4@gmail.com>
2025-03-22 01:41:16 +0530

    update

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -1,25 +1,45 @@
 import { createContext } from "react";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const calculateAge = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
 
-    const calculateAge = (dob) =>{
-        const today = new Date()
-        const birthDate = new Date(dob)
+    let age = today.getFullYear() - birthDate.getFullYear();
+    return age;
+  };
 
-        let age = today.getFullYear() - birthDate.getFullYear()
-        return age
+  const months = [
+    "",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-    }
-
-    const value = {
-        calculateAge
-    }
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split("_");
     return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
-export default AppContextProvider
+      dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    );
+  };
+
+  const value = {
+    calculateAge,
+    slotDateFormat
+  };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+export default AppContextProvider;
