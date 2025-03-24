0c8d247b8d277a9ec3661f2bc74ad376c5efb565
Sakshi Ajabe <sakshiajabe4@gmail.com>
2025-03-24 21:33:44 +0530

    update

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -3,13 +3,18 @@ import { createContext } from "react";
 export const DoctorContext = createContext()
 
 const DoctorContextProvider = (props) => {
+
+    const BackedUrl = import.meta.env.VITE_BACKEND_URL
+
     const value = {
 
     }
     return (
+        
         <DoctorContext.Provider value={value}>
             {props.children}
         </DoctorContext.Provider>
+
     )
 }
 export default DoctorContextProvider
