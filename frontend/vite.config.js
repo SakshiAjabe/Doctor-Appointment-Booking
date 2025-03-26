dfd9f0dad0dd46b426301f03ace66f84a8784c93
Sakshi Ajabe <sakshiajabe4@gmail.com>
2025-03-26 19:25:34 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "./",
+//   server:{port:5173}
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  server:{port:5173}
+  base: "./", // Ensures correct paths in index.html
+  server: { port: 5173 }
 })
